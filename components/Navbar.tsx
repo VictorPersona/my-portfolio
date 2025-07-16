b30e8c738c1f544771cf23ac5574b6e3f29566c0
VictorPersona <gauravtupe00@gmail.com>
2025-07-16 19:31:25 +0530

    Started with portfolio

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function Navbar(){
+    return(
+        <nav className='bg-white shadow-md py-4 px-6 sticky top-0 z-50'>
+            <div className="max-w-6xl mx-auto flex justify-between items-center">
+                
+            </div>
+        </nav>
+    )
+}
